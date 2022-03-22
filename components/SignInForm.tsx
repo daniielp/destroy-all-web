2b8a7c1be9eaf594bcd2ef70e047af5728da54c7
shorcy <59348193+shorcy@users.noreply.github.com>
2022-03-22 14:43:13 +0100

    update article

--- components/SignInForm.tsx
+++ components/SignInForm.tsx
@@ -1,29 +1,41 @@
 import { useState, useEffect } from "react";
-import { Button, TextField } from "@mui/material";
+import { Button, FormLabel, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import faker from "faker";
 
+interface SignInProps {
+  step: number
+}
 
-const SignInForm = () => {
+const SignInForm = ({ step = 1 }: SignInProps) => {
   var randomUsername = faker.internet.userName();
   const [errorText, setErrorText] = useState("");
 
   const handleClick = () => {
     var randomUsername = faker.internet.userName()
-    setErrorText("This password is already being used by " + randomUsername )
+    setErrorText("This password is already being used by " + randomUsername)
   }
 
   useEffect(() => {
     var randomUsername = faker.internet.userName()
-    setErrorText("This password is already being used by " + randomUsername )
+    setErrorText("This password is already being used by " + randomUsername)
   }, [])
+
+  const yearList = Array(12)
 
   return (
     <Box sx={{ maxWidth: 450, mx: "auto" }}>
+      <Typography align="center" variant="h5" component="h3" gutterBottom> Sign in</Typography>
       <TextField label="Username" fullWidth margin="normal" variant="filled" defaultValue="Nico.Anderson86" />
       <TextField label="Password" fullWidth type="text" error helperText={errorText} margin="normal" variant="filled" defaultValue="Password123" />
-      <Button onClick={handleClick}>Submit</Button>
+      {step > 1 ? (
+        <>
+          <TextField label="Credit Card Number*" type="number" fullWidth margin="none" variant="filled" helperText="*This field is very secure" />
+          
+        </>
+      ) : null}
+      <Button onClick={handleClick}>{step == 3 ? "Are you sure you want to submit?" : "Submit"}</Button>
     </Box>
   );
 };
--- data/articles.ts
+++ data/articles.ts
@@ -1,8 +1,8 @@
 const data = [
     {
-        "title": "How to create a sign in form",
-        "description": "When it comes to creating a sing in form, it can be done in a lot of different ways. This guide will show you some of the pitfalls and help you create better sign in forms.",
-        "slug": "how-to-create-a-sign-in-form",
+        "title": "How to destroy a sign in form",
+        "description": "We can all find examples on the internet of what people think is the best way to create a sign-in form. But how can we destroy it? That's what I'm going to find out.",
+        "slug": "how-to-destroy-a-sign-in-form",
         "tags": ["ui/ux", "forms"],
         "date": new Date("2022-3-21"),
     },
