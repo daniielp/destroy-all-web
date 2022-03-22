02aecb4b18771cdaf8e3361e8af0d9df5f0387be
shorcy <59348193+shorcy@users.noreply.github.com>
2022-03-22 10:12:38 +0100

    fix mdx

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-/** @type {import('next').NextConfig} */
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [],
     rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
+    providerImportSource: "@mdx-js/react"
   },
-  providerImportSource: "@mdx-js/react"
 })
 module.exports = withMDX({
   // Append the default value with md extensions
